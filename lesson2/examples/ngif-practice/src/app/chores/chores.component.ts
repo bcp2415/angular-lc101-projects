f8c5b62b6d7de91f0c9d15ba40a11c0b6862a007
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-12-08 06:18:11 -0600

    adds target image but doesn't display properly yet

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'chores',
-   templateUrl: './chores.component.html',
-   styleUrls: ['./chores.component.css']
+  selector: "chores",
+  templateUrl: "./chores.component.html",
+  styleUrls: ["./chores.component.css"],
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+  chores = [
+    "Empty dishwasher",
+    "Complete LaunchCode prep work",
+    "Buy groceries",
+  ];
+  finishedChores = [
+    "Gave woodchuck his vitamins",
+    "Sent dog out for beer",
+    "Made coffee",
+    "Took nap",
+  ];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  targetImage = "../assets/target.png";
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
-
+  ngOnInit() {}
 }
