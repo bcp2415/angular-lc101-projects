901623653d32e5b24b718369aebbb2ad36fc0b5e
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-12-12 19:42:12 -0600

    finishes exercises 30.6.3.1

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title = "Exercises: Angular Lesson 3";
 
-  color = 'green';
+  color = "green";
   height = 0;
   width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  message = "Space shuttle ready for takeoff!";
 }
