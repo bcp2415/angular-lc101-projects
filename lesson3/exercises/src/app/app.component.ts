c91a3f5e2bc183f02e28efdbf1e227eaae5021c6
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-12-13 12:16:38 -0600

    finished old requirements for lesson3 exercises

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -25,4 +25,46 @@ export class AppComponent {
       this.message = "Shuttle in flight.";
     }
   }
+
+  handleLanding(rocketImage) {
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    this.message = "The shuttle has landed.";
+    this.color = "green";
+    this.height = 0;
+    rocketImage.style.bottom = "0px";
+  }
+
+  handleAbort(rocketImage) {
+    let result = window.confirm("Are you sure you want to abort the mission?");
+
+    if (result) {
+      this.message = "Mission aborted.";
+      this.color = "red";
+      this.height = 0;
+      rocketImage.style.bottom = "0px";
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    } else if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    }
+
+
+
+  }  
 }
